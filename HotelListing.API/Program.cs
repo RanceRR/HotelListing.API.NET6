2770b174b3152c2b49a50e92d23461c8552a6509
Rance Richardson <rancer@buffiniandcompany.com>
2024-12-10 10:41:43 -0800

    Add project files.

---
+++ HotelListing.API/Program.cs
@@ -0,0 +1,40 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Add CORS (Cross-Origin Resource Sharing) services.  CORS is a W3C standard that allows a server to relax the same-origin policy. 
+//Using CORS, a server can explicitly allow some cross-origin requests while rejecting others (resources from a different domain).
+builder.Services.AddCors(options => {
+    options.AddPolicy("AllowAll", b => b.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+    );
+});
+
+builder.Host.UseSerilog((ctx, lc) => lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
